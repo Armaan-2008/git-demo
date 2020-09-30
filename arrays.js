13161e3c1742f128d72fcede11002bbbbf2181a9
Ramautar <r.ramautar@novi-education.nl>
2020-09-30 10:14:41 +0200

    First commit

--- arrays.js
+++ arrays.js
@@ -2,12 +2,16 @@
 // Opdracht 1
 // --------------------------
 
-let names = ["Henk", "Piet", "Fred", "Joop"];
+const names = ["Henk", "Piet", "Fred", "Joop"];
 
 // Zorg ervoor dat er -"je" achter iedere naam wordt geplakt
 // geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
 
+for (let i = 0; i < names.length; i++){
+    names[i] = names[i]+"je";
+}
 
+console.log(names);
 
 // --------------------------
 // Opdracht 2
@@ -18,13 +22,21 @@ const numbers = [2, 4, 5, 29, 38];
 // Vermenigvuldig iedere waarde in de array met 3
 // geeft: [ 6, 12, 15, 87, 114 ];
 
-
-
+for (let i = 0; i < numbers.length; i++){
+    numbers[i] = numbers[i]*3;
+}
+console.log(numbers);
 // --------------------------
 // Opdracht 3
 // --------------------------
 
 const figures = [4, 6, 12, 34, 1];
+
+for (let i=0; i < figures.length; i++){
+    figures[i] = figures[i]+3;
+}
+
+console.log(figures);
 
 // Tel 3 op bij ieder item in de array
 // geeft: [ 7, 9, 15, 37, 4 ]
